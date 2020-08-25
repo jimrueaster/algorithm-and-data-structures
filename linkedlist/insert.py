4d706aef4649581060b15a35e9afa00718c51c9d
JinRu Xian <jimru.xian@tenfoldtech.com>
2020-08-25 23:37:06 +0800

    单向链表: 粗略实现 insert

---
+++ linkedlist/insert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+from linkedlist.node import OneDirectNode
+
+n0 = OneDirectNode()
+n1 = OneDirectNode('aaaa')
+n2 = OneDirectNode('bbbb')
+n3 = OneDirectNode('cccc')
+
+n0.set_next(n1)
+n1.set_next(n2)
+n2.set_next(n3)
+
+new = OneDirectNode('new')
+
+
+def insert_after(head, index, new_node):
+    i = 0
+    node = head
+    while True:
+        if i == index:
+            old_next = node.next()
+            new_node.set_next(old_next)
+            node.set_next(new_node)
+        if None is node.next():
+            break
+        i += 1
+        node = node.next()
+
+
+def print_one_d_linked_list(head):
+    node = head
+    while True:
+        print(node.content())
+        if None is node.next():
+            break
+        node = node.next()
+
+
+insert_after(n0, 3, new)
+print_one_d_linked_list(n0)
